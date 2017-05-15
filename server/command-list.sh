1159d16f3b2e483ebdcee16425754b44f75b92aa
Error 404 - Name not found <bcr25939@kyois.com>
2017-05-15 13:54:46 -0400

    Update command-list.sh

--- server/command-list.sh
+++ server/command-list.sh
@@ -1,11 +1,7 @@
 echo "welcome to Lysergic backup server"
 echo "command-list.sh"
 echo
-echo "laptop-search		search laptop files NOT system logs"
-echo "desktop-search		search desktop files NOT system logs"
-echo
-echo "laptop-logs		search laptop system logs"
-echo "desktop-logs		search desktop system logs"
+echo "search-backups  search all backups"
+echo "search-logs     search all logs"
 echo "capacity    		show storage used and availible"
-echo
-echo "nuke			deletes all backups and logs"
+echo "nuke			      deletes all backups and logs"
